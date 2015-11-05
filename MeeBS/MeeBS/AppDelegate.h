07b35aa71532f61c8be48df15b72f4335aed419a
Mee <Lee@yangfanqihang.local>
2015-11-05 10:36:02 +0800

    创建一个项目

---
+++ MeeBS/MeeBS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeeBS
+//
+//  Created by 扬帆起航 on 15/11/5.
+//  Copyright © 2015年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
